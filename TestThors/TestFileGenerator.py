20e4aeb7901fe0521d3eb93f7337c97e299a61b4
Medic5700 <super_alias_the_first@hotmail.com>
2017-07-10 20:47:39 -0400

    Test file generator, used for testing

---
+++ TestThors/TestFileGenerator.py
@@ -0,0 +1,45 @@
+'''Creates the test files/directories used for testing
+Note: emphisis is on readability, not performance here
+'''
+import os
+
+#Single file, small file size, even, simple name
+print("Creating: Single file, small file size, even, simple name")
+f1 = open("0001.tmp", "bw")
+for i in range(0, 4*1024):
+    f1.write(bytes([i % 256]))
+f1.close()
+
+#Single file, small file size, odd, simple name
+print("Creating: Single file, small file size, odd, simple name")
+f1 = open("0002.tmp", "bw")
+for i in range(0, 4*1024 + 10):
+    f1.write(bytes([i % 256]))
+f1.close()
+
+#Single file, medium file size, even, simple name
+print("Creating: Single file, medium file size, even, simple name")
+f1 = open("0003.tmp", "bw")
+for i in range(0, 512*1024):
+    f1.write(bytes([i % 256]))
+f1.close()
+
+#Single file, medium file size, even, simple name
+print("Creating: Single file, medium file size, even, simple name")
+f1 = open("0004.tmp", "bw")
+for i in range(0, 64*1024*1024):
+    f1.write(bytes([i % 256]))
+f1.close()
+
+#Multiple files, medium file size, even, simple name
+print("Creating: Multiple files, medium file size, even, simple name")
+if not os.path.exists("tmp1"):
+    os.makedirs("tmp1")
+f1 = open("tmp1/0001.tmp", "bw")
+for i in range(0, 64*1024*1024):
+    f1.write(bytes([i % 256]))
+f1.close()
+f1 = open("tmp1/0002.tmp", "bw")
+for i in range(0, 64*1024*1024):
+    f1.write(bytes([i % 256]))
+f1.close()
