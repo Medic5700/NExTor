21fcba71593742a4e73fb37003f936fb11004c4f
Medic5700 <super_alias_the_first@hotmail.com>
2018-05-11 22:50:48 -0400

    half implementation of torrent creation

--- Prototype1.py
+++ Prototype1.py
@@ -355,6 +355,166 @@ for i in range(0, 89):
     f1._loadBuffer(i*256*1024)
 print("finished test")
 
+class CreateTorrent:
+    """All the machinery neccasary to create a torrent from a file/directory path"""
+    
+    def walk(path='.'):
+        """Takes a path, and recursivly transverses file tree, returns list of filepaths (files only)(empty directories not listed)"""
+        output = []
+        for i in os.listdir(path):
+            if os.path.isfile(os.path.join(path, i)):
+                output.append(os.path.join(path, i))
+            if os.path.isdir(os.path.join(path, i)):
+                temp = walk(os.path.join(path, i)) #recursive call
+                for j in temp:
+                    output.append(os.path.join(j))
+        return output
+    
+    def pieceSizeCalc(torrentSize):
+        """Takes a (int)size, returns a (int) peicessize"""
+        '''Every time the torrentSize x4, the piece size x2'''
+        # min block size 256*1024
+        import math
+        pieceSize = 2**(18 + math.floor(max(math.log2(torrentSize) - 26, 0) // 2))
+        return pieceSize
+    
+    def hashFile(filepath):
+        """Takes a file path, returns a byte array (64 bytes) representing the sha3_512 hash of the file"""
+        import hashlib
+        h = hashlib.sha3_512()
+        f1 = open(filepath, 'br')
+        #parses data 64MB at a time, to avoid loading the entirety of a file into memory
+        data = f1.read(64*1024*1024)
+        while len(data) == 64*1024*1024:
+            h.update(data)
+            data = f1.read(64*1024*1024)
+        f1.close()
+        if len(data) > 0:
+            h.update(data)
+        return h.digest()
+    
+    class TorFile:
+        def __init__(self, filePath, fileSize):
+            self.filePath = filePath
+            self.fileSize = fileSize
+            self.start = None
+            self.fileHash = None
+            self.ID = None
+            
+        def __repr__(self):
+            return "(" + self.filePath + ", " + str(self.fileSize) + ", " + str(self.start) + ")"    
+    
+    def fileOrganizer(pathList):
+        """Takes a list of file paths, returns an ordered array of file objects representing the files and position in the overall torrent"""
+        import os
+        import math
+        
+        #sort files by size
+        fileList = []
+        for i in pathList:
+            if os.path.getsize(i) == 0:
+                continue
+            if os.path.isdir(i) == True:
+                continue
+            fileList.append(TorFile(i, os.path.getsize(i)))
+        
+        torrentSize = 0
+        for i in fileList:
+            torrentSize += i.fileSize
+            
+        pieceSize = pieceSizeCalc(torrentSize)
+        
+        torrentFullSize = 0
+        for i in fileList:
+            torrentFullSize += pieceSize * math.ceil(i.fileSize/pieceSize)
+        
+        #sort the files into large, medium, and small sizeed files
+        # http://www.wolframalpha.com/input/?i=Plot%5Bx%2F(ceil(x%2F(256*1024))*1024*256),+%7Bx,+0,2*1024*1024%7D%5D
+        largeFiles = []
+        mediumFiles = []
+        smallFiles = []
+        for i in fileList:
+            if i.fileSize > 4 * pieceSize:
+                largeFiles.append(i)
+            elif i.fileSize < pieceSize // 16:
+                smallFiles.append(i)
+            else:
+                mediumFiles.append(i)
+    
+        #sorts list into acending order, makes it more efficent to 'pop' from the list
+        largeFiles = sorted(largeFiles, key=lambda TorFile: TorFile.fileSize)
+        mediumFiles = sorted(mediumFiles, key=lambda TorFile: TorFile.fileSize)
+        smallFiles = sorted(smallFiles, key=lambda TorFile: TorFile.fileSize)
+        
+        startList = {}
+    
+        bookmark = 0 #used for showing next free byte
+        piecemark = 0 #used for showing next free chunk
+        
+        # files are aligned to blocks (IE: files do not share blocks with other files)
+        while len(largeFiles) != 0:
+            #print("\t", "large", "estimated end", largeFiles[-1].fileSize - 1 + bookmark, "bookmark", bookmark, "piecemark", piecemark)
+            temp = largeFiles.pop()
+            startList[bookmark] = temp
+            temp.start = bookmark
+            piecemark = math.ceil((bookmark + temp.fileSize)/pieceSize) * pieceSize
+            bookmark = piecemark
+            
+        # files are partially aligned to blocks, but any leftover space is filled with other files
+        while len(mediumFiles) != 0:
+            stuffing = True
+            limiter = bookmark + math.ceil(mediumFiles[-1].fileSize/pieceSize) * pieceSize
+            #print("Bookmark", bookmark, "limiter", limiter)
+            
+            while len(mediumFiles) != 0:
+                #print("\t", "medium-medium", "estimated end", mediumFiles[-1].fileSize - 1 + bookmark, "limiter", limiter, "bookmark", bookmark, "piecemark", piecemark)
+                if mediumFiles[-1].fileSize - 1 + bookmark > limiter:
+                    #print("\t\t", "breaking 1")
+                    break
+                temp = mediumFiles.pop()
+                startList[bookmark] = temp
+                temp.start = bookmark
+                piecemark = math.ceil((bookmark + temp.fileSize - 1)/pieceSize) * pieceSize
+                bookmark = bookmark + temp.fileSize
+            
+            while (len(smallFiles) != 0):
+                #print("\t", "medium-small", "estimated end", smallFiles[-1].fileSize - 1 + bookmark, "limiter", limiter, "bookmark", bookmark, "piecemark", piecemark)
+                if smallFiles[-1].fileSize -1 + bookmark > limiter:
+                    #print("\t\t", "breaking 1")
+                    break            
+                temp = smallFiles.pop()
+                startList[bookmark] = temp
+                temp.start = bookmark
+                piecemark = math.ceil((bookmark + temp.fileSize - 1)/pieceSize) * pieceSize
+                bookmark = bookmark + temp.fileSize
+            
+            bookmark = piecemark
+            
+        #throws all the small files together, with no chunk alignments
+        while (len(smallFiles) != 0):
+            #print("\t", "small", "estimated end", smallFiles[-1].fileSize - 1 + bookmark, "bookmark", bookmark, "piecemark", piecemark)
+            temp = smallFiles.pop()
+            startList[bookmark] = temp
+            temp.start = bookmark
+            piecemark = math.ceil((bookmark + temp.fileSize - 1)/pieceSize) * pieceSize
+            bookmark = bookmark + temp.fileSize    
+        
+        return startList
+    
+    def createMetaData(rootPath):
+        """Takes a file or directory path, returns a data structure representing the metadata of a torrent"""
+        
+        
+    
+    def MetaData2File(metaData):
+        """Takes the data strcture representing the metadata of a torrent, returns a byte stream representing a .json file"""
+        pass
+    
+    def File2MetaData(filePath):
+        """Takes a file path (or byte steam, still deciding), returns the data strcture representing the metadata of a torrent"""
+        pass
+        
+test = CreateTorrent.createMetaData("./TestThors/tmp4")
 
 
 '''
